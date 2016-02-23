37f5f0f93137603a70efaa271a66207e2376c104
Michelle Krejci <michelle.krejci@pantheon.io>
2016-02-22 16:48:55 -0800

    Using tornado.

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ import connexion
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.add_api('swagger.json', arguments={'title': ''})
-    app.run(port=8080)
+    app.run(server='tornado', port=8080)
